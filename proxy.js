cf4b83f0ac5d384f3880dae02818a0140f31d2ce
Jeff Marshall <jeff@classy.co>
2014-09-28 01:13:59 -0700

    stable

--- proxy.js
+++ proxy.js
@@ -1,31 +1,40 @@
 var http = require('http');
+var connect = require('connect');
 var http_proxy = require('http-proxy');
-var http_auth = require('http-auth');
+
+var target = [
+  'http://',
+  process.env['ES_PORT_9200_TCP_ADDR'],
+  ':',
+  process.env['ES_PORT_9200_TCP_PORT']
+].join('')
 
 var proxy = http_proxy.createProxyServer({
-  target: [
-    'http://',
-    process.env['ES_PORT_9200_TCP_ADDR'],
-    ':',
-    process.env['ES_PORT_9200_TCP_PORT']
-  ].join('')
+  'target': target
 })
 
+var http_auth = require('http-auth');
 var basic_auth = http_auth.basic({
   realm: 'Causemap Search API',
   file: [ __dirname, 'search_api.htpasswd'].join('/')
 })
 
-var allow_whatever_for_search = function(req, res, next){
-  // TODO: check if the path ends in '_search'
-  console.log(req);
-  next();
-}
+var app = connect()
+  .use(function(req, res, next){
+    // check if the request is for a search endpoint
+    if (/_search$/.test(req.url.split('?')[0])){
+      // send it along to the proxy
+      return proxy.web(req, res);
+    }
+     next()
+  })
+  .use(http_auth.connect(basic_auth))
+  .use(function(req, res, next){
+    return proxy.web(req, res);
+  })
 
 
-http.createServer(allow_whatever_for_search, function(req, res){
-  return proxy.web(req, res);
-}).listen(9200)
+http.createServer(app).listen(9200)
 
 
 console.log('listening on port 9200')
