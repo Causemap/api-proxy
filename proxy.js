6d341a620937ee1f09e126e55a1de55c0fc126de
Jeff Marshall <jeff@classy.co>
2014-11-10 18:36:44 -0800

    using cluster module for multiple core stuff

--- proxy.js
+++ proxy.js
@@ -1,50 +1,64 @@
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
 var http = require('http');
 var connect = require('connect');
 var http_proxy = require('http-proxy');
 
-var target = [
-  'http://',
-  process.env['ES_PORT_9200_TCP_ADDR'],
-  ':',
-  process.env['ES_PORT_9200_TCP_PORT']
-].join('')
+if (cluster.isMaster) {
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-var proxy = http_proxy.createProxyServer({
-  'target': target
-})
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
+  });
+} else {
+  var target = [
+    'http://',
+    process.env['ES_PORT_9200_TCP_ADDR'],
+    ':',
+    process.env['ES_PORT_9200_TCP_PORT']
+  ].join('')
 
-var http_auth = require('http-auth');
-var basic_auth = http_auth.basic({
-  realm: 'Causemap Search API',
-  file: [ __dirname, 'search_api.htpasswd'].join('/')
-})
+  var proxy = http_proxy.createProxyServer({
+    'target': target
+  })
 
-function handle_error(error){
-  console.error(error)
+  var http_auth = require('http-auth');
+  var basic_auth = http_auth.basic({
+    realm: 'Causemap Search API',
+    file: [ __dirname, 'search_api.htpasswd'].join('/')
+  })
+
+  function handle_error(error){
+    console.error(error)
+  }
+
+  var app = connect()
+    .use(function(req, res, next){
+      req.socket.on("error", handle_error);
+      res.socket.on("error", handle_error);
+
+      // check if the request is for a search endpoint or a GET/OPTIONS request
+      if (
+        /_search$/.test(req.url.split('?')[0]) ||
+        ['GET', 'OPTIONS'].indexOf(req.method) != -1
+      ){
+        // send it along to the proxy
+        return proxy.web(req, res);
+      }
+       next()
+    })
+    .use(http_auth.connect(basic_auth))
+    .use(function(req, res, next){
+      return proxy.web(req, res);
+    })
+
+
+  http.createServer(app).listen(9200)
+
+
+  console.log('listening on port 9200')
 }
 
-var app = connect()
-  .use(function(req, res, next){
-    req.socket.on("error", handle_error);
-    res.socket.on("error", handle_error);
-
-    // check if the request is for a search endpoint or a GET/OPTIONS request
-    if (
-      /_search$/.test(req.url.split('?')[0]) ||
-      ['GET', 'OPTIONS'].indexOf(req.method) != -1
-    ){
-      // send it along to the proxy
-      return proxy.web(req, res);
-    }
-     next()
-  })
-  .use(http_auth.connect(basic_auth))
-  .use(function(req, res, next){
-    return proxy.web(req, res);
-  })
-
-
-http.createServer(app).listen(9200)
-
-
-console.log('listening on port 9200')
